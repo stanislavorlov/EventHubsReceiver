4e047b58e700758642a4dcc41f258f26763847d6
Stanislav Orlov <stanislav.orlov@external.cnhind.com>
2020-11-26 17:12:02 +0200

    Initial commit

---
+++ EHReaderConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using Azure.Messaging.EventHubs.Consumer;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EHReaderConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListenEvenHub().Wait();
+        }
+
+        static async Task ListenEvenHub()
+        {
+            var connectionString = "";
+            var eventHubName = "";
+            var consumerGroup = "";
+
+            await using (var client = new EventHubConsumerClient(consumerGroup, connectionString, eventHubName))
+            {
+                using var cancellationSource = new CancellationTokenSource();
+                cancellationSource.CancelAfter(TimeSpan.FromSeconds(45));
+
+                await foreach (PartitionEvent partitionEvent in client.ReadEventsAsync(cancellationSource.Token))
+                {
+                    Console.WriteLine(Encoding.UTF8.GetString(partitionEvent.Data.Body.ToArray()));
+                }
+            }
+        }
+    }
+}
